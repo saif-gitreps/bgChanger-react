e1b51fe136f40f4b7ca8cfdd25e4d6e7f241f129
Saif <droconos99@gmail.com>
2024-02-27 16:54:42 +0600

    added some changes

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -5,8 +5,8 @@ function App() {
    const [color, setColor] = useState("olive");
 
    return (
-      <div className="w-full h-screen duration-200" style={{ background: color }}>
-         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-20 px-2">
+      <div className="w-full h-screen duration-1000" style={{ background: color }}>
+         <div className="fixed flex flex-wrap justify-center bottom-10 inset-x-20 px-2">
             <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 py-2 rounded-3xl text-slate-200">
                <Button color="red" setColor={setColor} />
                <Button color="green" setColor={setColor} />
